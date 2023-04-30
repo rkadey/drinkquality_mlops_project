8b89bc7591ac29a570a9d1fb0f825bfbf8fcce00
Richard Kadey <richardkadey@gmail.com>
2023-04-30 23:03:49 +0000

    update get_data

--- src/get_data.py
+++ src/get_data.py
@@ -7,5 +7,12 @@ import pandas as pd
 import argparse
 
 
-if __name__=="__main__":
+
     
+
+    
+if __name__=="__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+
