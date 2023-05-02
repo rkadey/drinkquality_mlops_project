7e6f05d2aeb4e53499f003c8fc9bf2ab0bd22bf3
Richard Kadey <richardkadey@gmail.com>
2023-05-02 22:31:54 +0000

    updates

--- src/load_data.py
+++ src/load_data.py
@@ -2,13 +2,14 @@
 # saved it in the data/raw for further process
 import os
 import argparse
-from get_data import read_params, get_data
+from get_data import read_params_, get_data_
 
 def load_and_save(config_path):
-    config = read_params(config_path)
-    df = get_data(config_path)
-    new_cols = [col for col in df.columns]
-    print(new_cols)
+    config = read_params_(config_path)
+    df = get_data_(config_path)
+    #new_cols =  df.columns
+   # col.replace(" ", "_")
+    print(df)
 
     #raw_data_path= config["load_data"]["raw_dataset_csv"]
 
@@ -18,4 +19,4 @@ if __name__ =="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", default="params.yaml")
     parsed_args = args.parse_args()
-    load_and_save(config_path=parsed_args.config)
+    load_and_save(config_path = parsed_args.config)
